57d9ed86134fc66f25c1867883c94f4c110e74ad
Sasha Slacum <slacus08@gmail.com>
2017-07-24 12:08:46 -0400

    added function to pass arguments form user and return tweets, added npm for spotify but currently not working - need to debug

--- liri.js
+++ liri.js
@@ -4,12 +4,47 @@ var keys = require('./keys.js');
 // Use current folder .
 var Twitter = require('twitter');
 
-var client = new Twitter(keys.twitterKeys);
+var spotify = require('spotify');
 
+var getTweets = function() {
 
-var params = {screen_name: 'sashballs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+    var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: 'sashballs'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        // console.log(tweets);
+        for (var i=0; i < tweets.length; i++){
+        console.log(tweets[i].created_at);
+        console.log('');
+        console.log(tweets[i].text);
+        }
+      }
+    });
+}
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+    console.log(data);
 });
+
+var pick = function(caseData, functionData) {
+  switch(caseData) {
+    case 'my-tweets' :
+      getTweets();
+      break;
+    default:
+    console.log('LIRI does not know that');
+  }
+}
+
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
+
+//pass run the function, arguments from the user
+runThis(process.argv[2], process.argv[3]);
